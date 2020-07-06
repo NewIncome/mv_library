285c65d29b494aced4738a7e852372aecf772def
NewIncome <alfredo_cardenas@live.com>
2020-07-06 18:02:09 -0500

    Add default books: - remove html book layout elements - add some CSS for book info presentation - add add HTML elements to DOM according to default book info

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let myLibrary = [];
 let countId = 1
 
-function Book(author, title, numPages, read) {
+function Book(title, author, numPages, read = false) {
   this.id = countId
   this.author = author
   this.title = title
@@ -10,7 +10,8 @@ function Book(author, title, numPages, read) {
   countId++
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary(title, author, numPages, read = false) {
+  let book = new Book(title, author, numPages, read);
   if (myLibrary.push(book)) return true
 
   return false
@@ -26,6 +27,42 @@ function removeBookFromLibrary(book) {
   }
 }
 
+function displayDefaultBooks() {
+  addBookToLibrary('National Geographic: The Photographs', 'Leah Bendavid-Val', 336);
+  addBookToLibrary('Setting the Family Free', 'Eric D. Goodman', 310);
+  addBookToLibrary('Sea of Ink : A Creative Writing Anthology ', 'Niamh King', 248);
+  addBookToLibrary('What the Dinosaurs Did Last Night ', 'Refe Tuma and Susan Tuma', 112);
+}
+
 //let book= new Book(all elements)
 //if removeBookFromLibrary()
 //remove the book from DOM
+
+// DOM Manipulation
+function render() {
+  myLibrary.forEach(book => {
+    const div = document.createElement('div'); // to create the book div
+    div.classList.add('book');
+
+    const divTitle = document.createElement('h3'); // to create the book title
+    divTitle.innerHTML = book.title;
+    div.appendChild(divTitle);
+
+    const divAuthor = document.createElement('p'); // to create the author
+    divAuthor.innerHTML = book.author;
+    div.appendChild(divAuthor);
+
+    const divPages = document.createElement('p'); // to create the pages
+    divPages.innerHTML = book.numPages;
+    div.appendChild(divPages);
+
+    const divRead = document.createElement('p'); // to create the read text
+    divRead.innerHTML = book.read ? 'Read' : 'Not read';
+    div.appendChild(divRead);
+
+    document.getElementById('bookshelf').appendChild(div);
+  });
+}
+
+displayDefaultBooks();
+render();
