965c8ef9a86863363de3540c4322c4f7903c025b
NewIncome <alfredo_cardenas@live.com>
2020-07-24 23:28:43 -0500

    Local Storage Functionality - Add localStorage functionality! (working perfectly!!) - Separate testing file for pending transformatoin to class usage - Pending to add a button to clear the localStorage

--- script.js
+++ script.js
@@ -1,68 +1,159 @@
-/* eslint-disable no-unused-vars, max-classes-per-file */
+/* eslint-disable no-unused-vars */
 
-// Global variable
-let bookNum = 1;
+const myLibrary = [];
+// localStorage.setItem('myLibrary', myLibrary);
+let countId = 1;
+const bookForm = document.getElementById('bookForm');
+const title = document.getElementById('formTitle');
+const author = document.getElementById('formAuthor');
+const pages = document.getElementById('formPages');
+const read = document.getElementById('formRead');
 
-// Class to create/have a Library
-class Library {
-  constructor(name) {
-    this.name = name;
-    this.collection = [];
+
+function Book(title, author, numPages, read = false) {
+  this.id = countId;
+  this.author = author;
+  this.title = title;
+  this.numPages = numPages;
+  this.read = read;
+  countId += 1;
+}
+
+function lStorageTOmLibrary(id) {
+  const keysVar = localStorage.getItem('keys').split(',');
+  const valueVar = localStorage.getItem(id).split(',');
+  console.log(valueVar);
+  const obj = {};
+  keysVar.forEach((key, i) => {
+    obj[key] = valueVar[i];
+  });
+  console.log('obj.id: ', obj.id);
+  if (myLibrary.push(obj)) return true;
+  return false;
+}
+
+function populateMyLib() {
+  const sortedLocalStorageKeys = Object.keys(localStorage).sort((a, b) => a - b);
+  sortedLocalStorageKeys.forEach((lKey) => {
+    console.log('lKey: ' + lKey);
+    if ((/\d/).test(lKey)) lStorageTOmLibrary(lKey);
+  });
+}
+
+function addBookToLibrary(title, author, numPages, read = false) {
+  console.log('countId: ' + countId);
+  const book = new Book(title, author, numPages, read);
+  if (localStorage.length === 0) localStorage.setItem('keys', Object.keys(book)); // to get the att
+  localStorage.setItem('bookCount', countId); // save track of the current bookId count
+  localStorage.setItem(book.id, Object.values(book));
+  if (lStorageTOmLibrary(book.id)) return true;
+
+  return false;
+}
+
+// DOM Manipulation
+function render() {
+  const divBookShelf = document.getElementById('bookshelf');
+  while (divBookShelf.firstChild) {
+    divBookShelf.removeChild(divBookShelf.firstChild);
   }
+  const orderedLib = [...myLibrary];
+  orderedLib.reverse().forEach(book => {
+    const {
+      id, author, title, numPages, read,
+    } = book;
+    const bookContainer = document.createElement('div');
+    bookContainer.classList.add('book-container');
 
-  // Add a book to the Library obj
-  addBookToLibrary(book) {
-    if (this.collection.push(book)) return true;
-    return false;
+    const divBook = document.createElement('div'); // to create the book div
+    divBook.classList.add('book');
+
+    const divTitle = document.createElement('h3'); // to create the book title
+    divTitle.innerHTML = title;
+    divBook.appendChild(divTitle);
+
+    const divAuthor = document.createElement('p'); // to create the author
+    divAuthor.innerHTML = author;
+    divBook.appendChild(divAuthor);
+
+    const divPages = document.createElement('p'); // to create the pages
+    divPages.innerHTML = `${numPages} pages`;
+    divBook.appendChild(divPages);
+
+    const divRead = document.createElement('p'); // to create the read text
+    divRead.innerHTML = read ? 'Read' : 'Not read';
+    divBook.appendChild(divRead);
+
+
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove-button');
+    removeButton.innerHTML = 'Delete';
+    removeButton.setAttribute('onClick', `removeBookFromLibraryNlStorage(${id})`);
+
+    const readButton = document.createElement('button');
+    readButton.innerHTML = `Mark as ${read ? 'not read' : 'read'}`;
+    readButton.setAttribute('onClick', `readBook(${id})`);
+
+    bookContainer.appendChild(divBook);
+    bookContainer.appendChild(removeButton);
+    bookContainer.appendChild(readButton);
+
+    divBookShelf.appendChild(bookContainer);
+  });
+}
+
+function removeBookFromLibraryNlStorage(bookId) {
+  const index = myLibrary.findIndex(bookElem => +bookElem.id === bookId);
+  console.log(typeof index, index);
+  if (index !== -1) {
+    myLibrary.splice(index, 1);
+    localStorage.removeItem(index + 1);
+    render();
   }
 }
 
-// Class to create Books
-class Book {
-  constructor(author, title, numPages, read = false) {
-    this.id = bookNum;
-    this.author = author;
-    this.title = title;
-    this.numPages = numPages;
-    this.read = read;
-    bookNum += 1;
-  }
+const readBook = (bookId) => {
+  const foundBook = myLibrary.find(bookElem => bookElem.id === bookId);
+  foundBook.read = !foundBook.read;
+  render();
+};
 
-  // function to define if a book is read or not
-  readBook() {
-    this.read = !this.read;
+const toggleForm = () => {
+  if (bookForm.attributes.class.value === 'hidden') {
+    bookForm.attributes.class.value = 'block';
+  } else {
+    bookForm.attributes.class.value = 'hidden';
   }
+};
+
+function displayDefaultBooks() {
+  addBookToLibrary('National Geographic: The Photographs', 'Leah Bendavid-Val', 336);
+  addBookToLibrary('Setting the Family Free', 'Eric D. Goodman', 310);
+  addBookToLibrary('Sea of Ink : A Creative Writing Anthology ', 'Niamh King', 248);
+  addBookToLibrary('What the Dinosaurs Did Last Night ', 'Refe Tuma and Susan Tuma', 112);
+  render();
 }
 
-Book.prototype.varb = 0;
+const clearInputs = () => {
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  read.checked = false;
+};
 
-const lib = new Library();
-console.log(lib);
+function saveBook(e) {
+  e.preventDefault();
+  addBookToLibrary(title.value, author.value, pages.value, read.checked);
+  render();
+  clearInputs();
+}
 
-const b1 = new Book('', '', 11);
-const b2 = new Book('', '', 12);
-console.log(b1, b2);
+bookForm.addEventListener('submit', saveBook);
 
-/*
-let lib = new Library();
-*/
-
-// ------------------- DOM -------------------
-// Class to create a DOM element needed for the render() in HTML
-// class Dom {
-//   constructor(name, tag) {
-//     this.name = name;
-//     return document.getElementById(tag);
-//   }
-
-//   // Methods available to these DOM objects
-//   addClass = (className) => {
-//     console.log(className);
-//     // this.classList.add(className);
-//   }
-// };
-// let bookForm = new Dom('book', 'bookForm');
-// console.log();
-
-// let t1 = new Dom('t1', 'bookForm');
-// t1.addClass('testing');
+if (localStorage.length === 0) {
+  displayDefaultBooks();
+} else {
+  countId = localStorage.getItem('bookCount');
+  populateMyLib();
+  render();
+}
